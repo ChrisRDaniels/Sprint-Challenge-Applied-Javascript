8966ab029fc0cd204c31bb72a07eede87d750383
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-09-06 10:19:28 -0600

    started tabs.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,27 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const NewCard = document.querySelector('.card');
+
+axios
+.get('https://lambda-times-backend.herokuapp.com/articles')
+.then( response => {
+    console.log(response.data)
+
+    NewCard.appendChild(cardCreator(response.data))
+})
+.catch(error => {
+    error
+})
+
+function cardCreator(data) {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const images = document.createElement('img');
+    const imgurl = document.createElement('a');
+    const author = document.createElement('span');
+
+    card.classList.add('card');
+    card
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,29 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const NewTab = document.querySelector('.tab');
+
+axios
+.get('https://lambda-times-backend.herokuapp.com/topics')
+.then( response => {
+    console.log(response.data)
+
+    NewCard.appendChild(cardCreator(response.data))
+})
+.catch(error => {
+    error
+})
+
+function TabCreator(data) {
+    const topics = document.createElement('div');
+    
+    
+    //classes
+    topics.classList.add('tab');
+
+    //data
+    topics.textContent = data.topics;
+
+    return NewTab
+}
